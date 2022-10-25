5f2098348cbaee338ddba6716791914fe8df9305
DevPrinceK <princesamuelpks@gmail.com>
2022-10-25 22:52:24 +0000

    implement django messages
    displays Django messages as closable alerts under the navigation bar

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.contrib import messages
 
 
 @login_required(login_url="/login/")
